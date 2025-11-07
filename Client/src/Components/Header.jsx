d6f464befacb8cca64d14c9d25a27bac1f9721c5
Srinivas Andukuri <ssai50520@gmail.com>
2025-11-08 00:53:13 +0530

    included use navigate import

--- Client/src/Components/Header.jsx
+++ Client/src/Components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { assets } from '../assets/assets'
 import { AppContext } from '../Context/AppContext'
 import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 
 const Header = () => {
