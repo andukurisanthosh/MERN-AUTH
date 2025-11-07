e26ca7bd8d9166a68eb9d914dece1d770330cd92
Srinivas Andukuri <ssai50520@gmail.com>
2025-11-08 00:16:22 +0530

    added farword slash to get in server js file

--- Server/Server.js
+++ Server/Server.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'client/dist')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
 });
 
